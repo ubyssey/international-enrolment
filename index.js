fb6a012c01d77eff28c60d7a9e004e4e838e4a15
C Lin <CYLin@Chris-MacBook-Pro.local>
2017-03-16 17:46:57 -0700

    Faculty line colors, labels, axis format

--- index.js
+++ index.js
@@ -36,24 +36,34 @@ d3.json('data/countries.json',
 
   }
 );
-
 // Chart
 
 var facultyColors = {
- 'enginnering': '#4f1c8b',
- 'music': '#e1b952',
- 'med': '#122544',
- 'sauder': '#808080',
- 'dentistry': '#9086ca',
- 'science': '#304b9c',
- 'arts': '#4f1c8b',
- 'kin': '#61a025',
- 'forestry': '#2c5509',
- 'LFS': '#000000'
+ "APSC": "#c22339",
+ "ARTS": "#4f1c8b",
+ "COMM": "#808080",
+ "DENT": "#9086ca",
+ "FRST": "#2c5509",
+ "LFS": "#000000",
+ "MEDI": "#122544",
+ "SCIE": "#304b9c"
+ //{"MUSC": "#e1b952"}  missing
+ //{"KINS": "#61a025"} missing 
+};
+
+var facultyNames = {
+  'APSC': 'Applied Science',
+  'ARTS': 'Arts',
+  'COMM': 'Commerce',
+  'DENT': 'Detistry',
+  'FRST': 'Forestry',
+  'LFS': 'Land and Food System',
+  'MEDI': 'Medicine',
+  'SCIE': 'Science',
 };
 
 var svg = d3.select("#chart"),
-    margin = {top: 20, right: 80, bottom: 30, left: 50},
+    margin = {top: 20, right: 150, bottom: 30, left: 80},
     width = svg.attr("width") - margin.left - margin.right,
     height = svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -61,8 +71,8 @@ var svg = d3.select("#chart"),
 var parseTime = d3.time.format("%Y%m%d");
 
 var x = d3.scale.linear().range([0, width]),
-    y = d3.scale.linear().range([height, 0]),
-    z = d3.scale.ordinal(d3.schemeCategory10);
+    y = d3.scale.linear().range([height, 0]);
+    //z = d3.scale.ordinal(d3.schemeCategory10);
 
 var line = d3.svg.line()
     .x(function(d) { return x(d.year); })
@@ -77,7 +87,9 @@ d3.json("data/faculties.json", function(error, faculties) {
 
   y.domain([0, 0.4]);
 
-  z.domain(faculties.map(function(c) { return c.faculty; }));
+  //z.domain(faculties.map(function(c) { return c.faculty; }));
+
+  // w.domain(faculties.map(function(c){return c.colors;}));
 
   g.append("g")
       .attr("class", "axis axis--x")
@@ -86,11 +98,12 @@ d3.json("data/faculties.json", function(error, faculties) {
 
   g.append("g")
       .attr("class", "axis axis--y")
-      .call(d3.svg.axis().scale(y).orient('left'))
+      .call(d3.svg.axis().scale(y).orient('left').tickFormat(d3.format(",%")))
     .append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
+      .attr("y", 0)
+      .attr("text-anchor", "end")
+      .attr("dy", "-50px")
       .attr("fill", "#000")
       .text("Percentage international students, %");
 
@@ -102,15 +115,18 @@ d3.json("data/faculties.json", function(error, faculties) {
   faculty.append("path")
       .attr("class", "line")
       .attr("d", function(d) { return line(d.values); })
-      .style("stroke", function(d) { return z(d.faculty); });
+      .style("stroke", function(d) { return facultyColors[d.faculty]; })
+      .style("stroke-width", '3px');
 
   faculty.append("text")
-      .datum(function(d) { return {id: d.faculty, value: d.values[d.values.length - 1]}; })
+      .datum(function(d) { return {faculty: d.faculty, value: d.values[d.values.length - 1]}; })
       .attr("transform", function(d) { return "translate(" + x(d.value.year) + "," + y(d.value.value) + ")"; })
       .attr("x", 3)
       .attr("dy", "0.35em")
       .style("font", "10px sans-serif")
-      .text(function(d) { return d.faculty; });
+      .text(function(d) { return facultyNames[d.faculty]; });
+
+
 });
 
 // function type(d, _, columns) {
